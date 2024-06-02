c36a7f9ee383362fda76d6e3fbbad847cdca61e3
shkim <seonflower91@gmail.com>
2024-06-02 21:27:12 +0900

    메서드기반 인가 관리자

--- src/main/java/io/security/springsecuritymaster/IndexController.java
+++ src/main/java/io/security/springsecuritymaster/IndexController.java
@@ -1,108 +1,28 @@
 package io.security.springsecuritymaster;
 
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.authentication.AnonymousAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.annotation.CurrentSecurityContext;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class IndexController {
 
-    private final SessionInfoService sessionInfoService;
+    private final DataService dataService;
 
-    @GetMapping("/")
-    public Authentication index(Authentication authentication){
-        return authentication;
+    @GetMapping("/user")
+    public String user(){
+        return dataService.getUser();
     }
 
-    @GetMapping("/sessionInfo")
-    public void sessionInfo() {
-        sessionInfoService.sessionInfo();
+    @GetMapping("/owner")
+    public Account owner(String name){
+        return dataService.getOwner(name);
+    }
+    @GetMapping("/display")
+    public String display(){
+        return dataService.display();
     }
 
-    @GetMapping("/home")
-    public String home() {
-        return "home";
-    }
-
-    @GetMapping("/loginPage")
-    public String loginPage() {
-        return "loginPage";
-    }
-
-    @GetMapping("/anonymous")
-    public String anonymous() {
-        return "anonymous";
-    }
-
-    @GetMapping("/authentication")
-    public String authentication(Authentication authentication) {
-        if (authentication instanceof AnonymousAuthenticationToken) {
-            return "is anonymous";
-        } else {
-            // org.springframework.security.authentication.UsernamePasswordAuthenticationToken
-            return "not anonymous";
-        }
-    }
-
-    @GetMapping("/anonymousContext")
-    public String anonymousContext(@CurrentSecurityContext SecurityContext securityContext) {
-        return securityContext.getAuthentication().getName();
-    }
-
-    @GetMapping("/invalidSessionUrl")
-    public String invalidSessionUrl(){
-        return "invalidSessionUrl";
-    }
-
-    @GetMapping("/expired")
-    public String expired(){
-        return "expired";
-    }
-
-    @GetMapping("/login")
-    public String login() {
-        return "loginPage";
-    }
-
-    @GetMapping("/denied")
-    public String denied() {
-        return "denied";
-    }
-
-    @PostMapping("/csrf")
-    public String csrf(){
-        return "csrf 적용";
-    }
-
-    @GetMapping("/csrfToken")
-    public String csrfToken(HttpServletRequest request){
-        CsrfToken csrfToken1 = (CsrfToken) request.getAttribute(CsrfToken.class.getName());
-        CsrfToken csrfToken2 = (CsrfToken) request.getAttribute("_csrf");
-        String token = csrfToken1.getToken();
-        return token;
-    }
-
-    @PostMapping("/formCsrf")
-    public CsrfToken formCsrf(CsrfToken csrfToken, String username, String password){
-        return csrfToken;
-    }
-
-    @PostMapping("/cookieCsrf")
-    public CsrfToken cookieCsrf(CsrfToken csrfToken){
-        return csrfToken;
-    }
-
-    @GetMapping("/secure")
-    public String secure(){
-        return "secure";
-    }
 
 }
--- src/main/java/io/security/springsecuritymaster/SecurityConfig.java
+++ src/main/java/io/security/springsecuritymaster/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
@@ -13,9 +14,9 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager;
 
 @EnableWebSecurity
+@EnableMethodSecurity(securedEnabled = true, jsr250Enabled = true)
 @Configuration
 public class SecurityConfig {
 
@@ -31,22 +32,17 @@ public class SecurityConfig {
 
         http
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/user").hasRole("USER")
-                        .requestMatchers("/db").access(new WebExpressionAuthorizationManager("hasRole('DB')"))
-                        .requestMatchers("/admin").hasAuthority("ROLE_ADMIN")
-                        .requestMatchers("/secure").access(new CustomAuthorizationManager())
                         .anyRequest().authenticated())
                 .formLogin(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable);
 
         return http.build();
     }
-
     @Bean
     public UserDetailsService userDetailsService(){
-        UserDetails user = User.withUsername("user").password("{noop}1111").authorities("USER").build();
-        UserDetails db = User.withUsername("db").password("{noop}1111").authorities("DB").build();
-        UserDetails admin = User.withUsername("admin").password("{noop}1111").authorities("ADMIN", "SECURE").build();
+        UserDetails user = User.withUsername("user").password("{noop}1111").roles("USER").build();
+        UserDetails db = User.withUsername("db").password("{noop}1111").roles("DB").build();
+        UserDetails admin = User.withUsername("admin").password("{noop}1111").roles("ADMIN","SECURE").build();
         return  new InMemoryUserDetailsManager(user, db, admin);
     }
 }
